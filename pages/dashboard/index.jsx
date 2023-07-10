c4449115ad143f4aef9954418141d489e906e82f
rohitrikhi <rohitrikhibtc@gmail.com>
2023-07-10 11:46:23 +0530

    Show message on dashboard

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,11 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import LeftSide from "../../containers/leftSidebar";
+import { LOCAL_STORAGE_KEYS } from "../../app/common/constants";
 
 const Dashboard = ({}) => {
+  const [accountType, setAccountType] = useState("")
+
+  useEffect(() => {
+   setAccountType(localStorage.getItem( LOCAL_STORAGE_KEYS.ACC_TYPE))
+  })
+  
   return (
     <Fragment>
       <div className="chitchat-container sidebar-toggle ">
         <LeftSide />
+        <div className="chitchat-main small-sidebar" id="content" style={{  textAlign: 'center', top: "300px"}}>
+          <h1>Welcome {accountType}</h1>
+        </div>
       </div>
     </Fragment>
   );
