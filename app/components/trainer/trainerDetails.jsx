98b2efbf24437988b887bd132410894fa76cf181
rohitrikhi <rohitrikhibtc@gmail.com>
2023-08-25 15:32:32 +0530

    [fix] accordion change

--- app/components/trainer/trainerDetails.jsx
+++ app/components/trainer/trainerDetails.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { X } from "react-feather";
-import Accordion from "../../common/accordion";
-import Carousel from "../../common/carousel";
-import { Message, TRAINER_AMOUNT_USD } from "../../common/constants";
+import React, {useEffect, useState} from 'react';
+import {X} from 'react-feather';
+import Accordion from '../../common/accordion';
+import Carousel from '../../common/carousel';
+import {Message, TRAINER_AMOUNT_USD} from '../../common/constants';
 
-const TrainerDetails = ({ onClose, element, trainerInfo }) => {
-  const [accordion, setAccordion] = useState({});
-
+const TrainerDetails = ({onClose, element, trainerInfo}) => {
+  const [accordion, setAccordion] = useState ({});
+  const [activeAccordion, setActiveAccordion] = useState ({});
   // TODO: showing dummy records, will replace it with actual records
   const mediaData = [
     {
-      url: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3274&q=80",
-      type: "image",
+      url: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3274&q=80',
+      type: 'image',
     },
     {
-      url: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3374&q=80",
-      type: "image",
+      url: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3374&q=80',
+      type: 'image',
     },
     // {
     //   url: 'https://youtu.be/mUxzKVrSAjs?si=v6oqi-0-rG7BJ5sk',
@@ -23,26 +23,29 @@ const TrainerDetails = ({ onClose, element, trainerInfo }) => {
     // },
   ];
 
-  useEffect(() => {
-    if (trainerInfo && trainerInfo.extraInfo) {
-      setAccordion(trainerInfo.extraInfo);
-    }
-  }, [trainerInfo]);
+  useEffect (
+    () => {
+      if (trainerInfo && trainerInfo.extraInfo) {
+        setAccordion (trainerInfo.extraInfo);
+      }
+    },
+    [trainerInfo]
+  );
 
   const accordionData = [
     {
       id: 1,
-      label: "Teaching Style",
+      label: 'Teaching Style',
       value: accordion.teaching_style,
     },
     {
       id: 2,
-      label: "Credentials & Affiliations",
+      label: 'Credentials & Affiliations',
       value: accordion.credentials_and_affiliations,
     },
     {
       id: 3,
-      label: "Curriculum",
+      label: 'Curriculum',
       value: accordion.curriculum,
     },
   ];
@@ -63,14 +66,30 @@ const TrainerDetails = ({ onClose, element, trainerInfo }) => {
           <p>
             {trainerInfo && trainerInfo.extraInfo
               ? trainerInfo.extraInfo.about
-              : "No data available... "}
+              : 'No data available... '}
           </p>
           {accordionData.length
-            ? accordionData.map((data, index) => {
+            ? accordionData.map ((data, index) => {
                 return (
                   <Accordion key={`accordion_${index}`} className="mb-5">
                     <Accordion.Item>
-                      <Accordion.Header>{data.label}</Accordion.Header>
+                      <Accordion.Header
+                        index={index}
+                        activeAccordion={activeAccordion}
+                        onAClick={() => {
+                          console.log( `active --- `, activeAccordion, index)
+                          if (activeAccordion[index]) {
+                            delete activeAccordion[index];
+                          } else if(!activeAccordion[index]) {
+                            activeAccordion[index] = true;
+                          } else {
+                            activeAccordion[index] = !activeAccordion[index];
+                          }
+                          setActiveAccordion (activeAccordion);
+                        }}
+                      >
+                        {data.label}
+                      </Accordion.Header>
                       <Accordion.Body>
                         {!data.value ? Message.notFound : data.value}
                       </Accordion.Body>
@@ -78,7 +97,7 @@ const TrainerDetails = ({ onClose, element, trainerInfo }) => {
                   </Accordion>
                 );
               })
-            : "No data found"}
+            : 'No data found'}
         </div>
         <div className="col-7">
           <Carousel
