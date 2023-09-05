64378380440cfe2ca2e85d5771bbc386411568ee
rohitrikhi <rohitrikhibtc@gmail.com>
2023-09-05 19:07:44 +0530

    enter to search trainer & search to click to show trainer

--- app/components/trainee/helper/searchableDropdown.jsx
+++ app/components/trainee/helper/searchableDropdown.jsx
@@ -1,5 +1,5 @@
-import {useEffect, useRef, useState} from 'react';
-import {Search} from 'react-feather';
+import { useEffect, useRef, useState } from "react";
+import { Search } from "react-feather";
 
 const SearchableDropdown = ({
   options,
@@ -10,36 +10,37 @@ const SearchableDropdown = ({
   handleChange,
   customClasses,
   selectedOption,
+  searchValue,
+  onSearchClick,
 }) => {
-  const [query, setQuery] = useState ('');
-  const [isOpen, setIsOpen] = useState (false);
-  const inputRef = useRef (null);
+  const [query, setQuery] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef(null);
 
-  useEffect (() => {
-    document.addEventListener ('click', toggle);
-    return () => document.removeEventListener ('click', toggle);
+  useEffect(() => {
+    document.addEventListener("click", toggle);
+    return () => document.removeEventListener("click", toggle);
   }, []);
 
-  const selectOption = option => {
-    setQuery (() => '');
-    handleChange (option[label]);
-    setIsOpen (isOpen => !isOpen);
+  const selectOption = (option) => {
+    handleChange(option[label]);
+    setIsOpen((isOpen) => !isOpen);
   };
 
-  function toggle (e) {
-    setIsOpen (e && e.target === inputRef.current);
+  function toggle(e) {
+    setIsOpen(e && e.target === inputRef.current);
   }
 
   const getDisplayValue = () => {
     if (query) return query;
     if (selectedVal) return selectedVal;
 
-    return '';
+    return "";
   };
 
-  const filter = options => {
-    const result = options.filter (
-      option => option[label].toLowerCase ().indexOf (query.toLowerCase ()) > -1
+  const filter = (options) => {
+    const result = options.filter(
+      (option) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1
     );
     return result;
   };
@@ -55,15 +56,20 @@ const SearchableDropdown = ({
                 className={customClasses.searchBar}
                 ref={inputRef}
                 type="text"
-                value={getDisplayValue ()}
+                value={getDisplayValue()}
                 name="searchTerm"
-                onChange={e => {
-                  setQuery (e.target.value);
-                  handleChange (null);
+                onChange={(e) => {
+                  setQuery(e.target.value);
+                  handleChange(null);
+                  searchValue(e.target.value);
                 }}
                 onClick={toggle}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    onSearchClick(query);
+                  }
+                }}
               />
-
             </div>
             {/* <div className={`arrow ${isOpen ? 'open' : ''}`} /> */}
           </div>
@@ -71,48 +77,60 @@ const SearchableDropdown = ({
         <div className="col-2 p-0 m-0">
           <button
             className={`btn btn-primary rounded-0 ${customClasses.searchButton}`}
+            onClick={() => onSearchClick(query)}
           >
-            {' '}
-            <Search />{' '}
+            {" "}
+            <Search />{" "}
           </button>
         </div>
       </div>
-      {query && query.length ?      <div className="row">
-        <div className="col-10 p-0 m-0">
-          <div className={`options ${(isOpen && filter (options).length) ? 'open' : ''}`}>
-            {filter (options).map ((option, index) => {
-              return (
-                <div
-                  onClick={() =>
-                    selectOption (option) || selectedOption (option)}
-                  className={`option ${option[label] === selectedVal ? 'selected' : ''}`}
-                  key={`${id}-${index}`}
-                >
-                  {/* {(option)} */}
-                  {option.isCategory
-                    ? <div className="row m-0 option-item">
+      {query && query.length ? (
+        <div className="row">
+          <div className="col-10 p-0 m-0">
+            <div
+              className={`options ${
+                isOpen && filter(options).length ? "open" : ""
+              }`}
+            >
+              {filter(options).map((option, index) => {
+                return (
+                  <div
+                    onClick={() =>
+                      selectOption(option) || selectedOption(option)
+                    }
+                    className={`option ${
+                      option[label] === selectedVal ? "selected" : ""
+                    }`}
+                    key={`${id}-${index}`}
+                  >
+                    {/* {(option)} */}
+                    {option.isCategory ? (
+                      <div className="row m-0 option-item">
                         <div className="col-10 ">{option.name}</div>
                         <div className="col-2 ">
-                          {' '}
-                          <b> Category </b>{' '}
+                          {" "}
+                          <b> Category </b>{" "}
                         </div>
                       </div>
-                    : <div className="row m-0 option-item">
+                    ) : (
+                      <div className="row m-0 option-item">
                         <div className="col-10 ">{option.name}</div>
                         <div className="col-2 ">
-                          {' '}
-                          <b> Trainer </b>{' '}
+                          {" "}
+                          <b> Trainer </b>{" "}
                         </div>
-                      </div>}
-                </div>
-              );
-            })}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
+          <div />
         </div>
-        <div />
-      </div>
- : <></>}
-
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
