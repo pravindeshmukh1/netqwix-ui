31edda8bc7a37c979fa4bd518999c21330d458c4
rohitrikhi <rohitrikhibtc@gmail.com>
2023-09-28 12:27:18 +0530

    [fix] build issue

--- utils/utils.js
+++ utils/utils.js
@@ -261,7 +261,7 @@ export class Utils {
   }
 
   static getRatings = (ratings) => {
-    const validRatings = ratings ? .filter(
+    const validRatings = ratings?.filter(
       (rating) =>
       rating &&
       rating.ratings &&
