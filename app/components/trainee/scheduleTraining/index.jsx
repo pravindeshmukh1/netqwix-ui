a7d1a1aa3ec797385f0693db1d15123d76d27d03
rohitrikhi <rohitrikhibtc@gmail.com>
2023-10-05 12:58:27 +0530

    fix price on session booking

--- app/components/trainee/scheduleTraining/index.jsx
+++ app/components/trainee/scheduleTraining/index.jsx
@@ -617,17 +617,13 @@ const ScheduleTraining = () => {
           ){" "}
         </h3>
         <h4 className="mt-3 mb-3">
-          Booking time: {moment(bookSessionPayload.booked_date).format("ll")} |
-          From: {bookSessionPayload.session_start_time} To:{" "}
-          {bookSessionPayload.session_end_time}
+          Booking time: {moment(bookSessionPayload?.booked_date).format("ll")} |
+          From: {bookSessionPayload?.session_start_time} To:{" "}
+          {bookSessionPayload?.session_end_time}
         </h4>
         <h4 className="mb-3">
           Price:
-          <b>
-            $
-            {bookSessionPayload?.trainer_info?.userInfo?.extraInfo
-              ?.hourly_rate || TRAINER_AMOUNT_USD}
-          </b>
+          <b>${bookSessionPayload?.charging_price}</b>
         </h4>
       </div>
     );
